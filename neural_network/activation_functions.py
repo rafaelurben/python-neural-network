ffe58d315583c704c4ad95c287c41c66e11ee685
rafaelurben <github@rafaelurben.ch>
2022-04-04 11:52:51 +0200

    Added sigmoid activation function

--- neural_network/activation_functions.py
+++ neural_network/activation_functions.py
@@ -1,24 +1,31 @@
 "Activation functions"
 
-def identity(x):
+import math
+
+def identity(t):
     "Identity function"
-    return x
+    return t
 
-def binary_step(x):
+def binary_step(t):
     "Binary step function"
-    return 1 if x >= 0 else 0
+    return 1 if t >= 0 else 0
 
-def relu(x):
+def relu(t):
     "Rectified linear unit"
-    return max(0, x)
+    return max(0, t)
 
-def leaky_relu(x):
+def leaky_relu(t):
     "Leaky rectified linear unit"
-    return 0.01*x if x < 0 else x
+    return 0.01*t if t < 0 else t
+
+def sigmoid(t):
+    "Sigmoid function"
+    return 1/(1+math.exp(-t))
 
 ACFUNCS = {
     "identity": identity,
     "binary_step": binary_step,
     "relu": relu,
     "leaky_relu": leaky_relu,
+    "sigmoid": sigmoid,
 }
