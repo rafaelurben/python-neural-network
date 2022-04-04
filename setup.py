902c7d7c7e64695405d0c28a2d4fef7e03d0e2bd
rafaelurben <github@rafaelurben.ch>
2022-04-04 10:47:24 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='neural_network',
+    version='0.0',
+    description='A neural network implementation in Python',
+    author='Rafael Urben',
+    author_email='github@rafaelurben.ch',
+    packages=['neural_network'],
+    install_requires=[],
+)
