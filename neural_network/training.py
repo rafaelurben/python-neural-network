af623a7b02b59ddab56d3806b404df913a5809da
rafaelurben <github@rafaelurben.ch>
2022-04-27 22:22:52 +0200

    Updated training.py

--- neural_network/training.py
+++ neural_network/training.py
@@ -13,9 +13,6 @@ class Genome():
         self.network = network
         self.obj = None
 
-    def clone(self):
-        return self.__class__(self.network.clone())
-
     def setup(self, *args, **kwargs):
         raise NotImplementedError
 
@@ -33,83 +30,99 @@ class Genome():
 class NeuroEvolution():
     "Base class for neural network training using NeuroEvolution"
 
-    def __init__(self, genome_class, *genome_setup_args, **genome_setup_kwargs):
-        self.learning_rate_base = 0.005
+    def __init__(self, genome_class, *genome_setup_args, name="neuro", folder="../data/", **genome_setup_kwargs):
+        self.learning_rate_base = 0.01
         self.learning_rate_factor = 0.95
         self.mutation_chance = 0.05
         self.population_size = 100
 
-        self.generation = 0
+        self.generation = -1
 
         self.genomes = []
         self.genome_class = genome_class
         self.genome_setup_args = genome_setup_args
         self.genome_setup_kwargs = genome_setup_kwargs
 
+        self.name = name
+        self.folder = folder
+
+    def _new_genome(self, network: NeuralNetwork):
+        genome = self.genome_class(network)
+        genome.setup(*self.genome_setup_args, **self.genome_setup_kwargs)
+        return genome
+
+    def _get_filename(self):
+        return f"neuro-{self.name}-gen{str(self.generation).zfill(3)}.json"
+
     def setup_from_scratch(self):
         for _ in range(self.population_size):
-            genome = self.genome_class(self.get_default_network())
-            genome.setup(*self.genome_setup_args, **self.genome_setup_kwargs)
-            self.genomes.append(genome)
+            self.genomes.append(self._new_genome(self._get_default_network()))
 
     def setup_from_file(self, filename):
-        with open(filename, "r", encoding="utf-8") as file:
+        print(f"Loading from file '{filename}'...", end=" ")
+
+        with open(self.folder+filename, "r", encoding="utf-8") as file:
             data = json.loads(file.read())
 
         self.generation = data["generation"]
         network = NeuralNetwork.from_dict(data["network"])
+        self.genomes.append(self._new_genome(network))
+        self.genomes.append(self._new_genome(network))
 
-        for _ in range(1, self.population_size):
-            genome = self.genome_class(network.clone())
-            genome.setup(*self.genome_setup_args, **self.genome_setup_kwargs)
-            self.genomes.append(genome)
+        for _ in range(self.population_size-2):
+            self.genomes.append(None)
 
-    def to_file(self, filename, network: NeuralNetwork):
+        self._generate_genomes(self._get_learning_rate())
+
+        print("Loaded!")
+
+    def to_file(self, filename:str=None):
+        filename = filename or self._get_filename()
+        
+        print(f"Saving to file '{filename}'...", end=" ")
+
         data = {
-            "network": network.to_dict(),
+            "network": self.genomes[0].network.to_dict(),
             "generation": self.generation,
         }
-        with open(filename, "w", encoding="utf-8") as file:
+        with open(self.folder+filename, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4)
 
+        print("Saved!")
 
     def run_generation(self):
-        learning_rate = self.get_learning_rate()
+        self.generation += 1
+        learning_rate = self._get_learning_rate()
 
         print(f"Generation {self.generation} generating... (learning rate: {learning_rate})", end=" ")
 
-        self.generate_genomes(learning_rate)
+        if self.generation > 0:
+            # No need to generate genomes in generation 0!
+            self._generate_genomes(learning_rate)
         
-        print(f"Done! Running training...")
+        print("Done! Running training...")
 
         for genome in tqdm(self.genomes, desc=f"Generation {self.generation}"):
             genome.run_evaluation(self.generation)
 
-        self.sort_genomes()
+        self._sort_genomes()
         highscore = self.genomes[0].score
 
         print(f"Generation {self.generation} ended! Highscore: {highscore}")
-        
-        self.generation += 1
 
-    def generate_genomes(self, learning_rate):
-        if self.generation == 0:
-            # No need to generate genomes in generation 0! Skipping...
-            return
-        
+    def _generate_genomes(self, learning_rate):
         for i in range(2, self.population_size):
-            orig_index = int(random.random())
+            orig = self.genomes[int(random.random())]
 
-            genome = self.genomes[orig_index].clone()
-            genome.network.mutate(learning_rate, self.mutation_chance)
-            genome.setup(*self.genome_setup_args, **self.genome_setup_kwargs)
-            self.genomes[i] = genome
+            network = orig.network.clone()
+            network.mutate(learning_rate, self.mutation_chance)
+            self.genomes[i] = self._new_genome(network)
 
-    def sort_genomes(self):
+    def _sort_genomes(self):
         self.genomes.sort(key=lambda genome: genome.score, reverse=True)
 
-    def get_learning_rate(self):
+    def _get_learning_rate(self):
         return self.learning_rate_base * (self.learning_rate_factor ** self.generation)
 
-    def get_default_network(self):
+    def _get_default_network(self):
         raise NotImplementedError
